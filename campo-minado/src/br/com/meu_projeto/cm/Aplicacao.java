26461e8eb11c34a1397cf7941a8c2509f42434cf
Fernando Narimatsu <fernando_narimatsu@hotmail.com>
2021-09-23 00:38:08 -0300

    final cm

--- campo-minado/src/br/com/meu_projeto/cm/Aplicacao.java
+++ campo-minado/src/br/com/meu_projeto/cm/Aplicacao.java
@@ -1,18 +1,14 @@
 package br.com.meu_projeto.cm;
 
 import br.com.meu_projeto.cm.modelo.Tabuleiro;
+import br.com.meu_projeto.cm.visao.TabuleiroConsole;
 
 public class Aplicacao {
 	
 	public static void main(String[] args) {
 		
-		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
-		
-		tabuleiro.abrir(3, 3);
-		tabuleiro.alternarMarcacao(4, 4);
-		tabuleiro.alternarMarcacao(4, 5);
-		
-		System.out.println(tabuleiro);
+		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);		
+		new TabuleiroConsole(tabuleiro);		
 	}
 
 }
